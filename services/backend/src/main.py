f92a05e088f487c5f6ab6c35df82f5fa1f243a37
Jan Egermaier <31566030+procesor2017@users.noreply.github.com>
2022-05-23 14:45:28 +0200

    Add connect frontend on backend

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -4,18 +4,25 @@ from fastapi import FastAPI, Request
 from .modules.create_matrix import choose_and_return_matrix
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-"""
+origins = [
+    "http://localhost",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:8080/",
+    "https://127.0.0.1:8080/",
+    ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-"""
+
 
 class Input(BaseModel):
     data: list
--- services/frontend/src/components/HelloWorld.vue
+++ services/frontend/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
       {{msg}}
-  <input v-model="msg"> 
-  <button @click="getTable">getTable</button>
+    <input v-model="myInput"> 
+    <button @click="getTable">getTable</button>
   </div>
 </template>
 
@@ -13,11 +13,16 @@ export default {
   data() {
     return {
       msg: '',
+      myInput: "",
     };
   },
   methods: {
     getTable() {
-      axios.get('/',)
+      let payload = {
+        "data": [{"0":"a","1":"aa","2":"aaa","3":"aaaa","4":"aaaaa"},{"0":"b","1":"bb","2":"bbb","3":"bbbb","4":"bbbbb"},{"4":"ccccc"},{"4":"ddddd"}]
+      }
+
+      axios.post('/getInformation', payload)
         .then((res) => {
           this.msg = res.data;
         })
